e7a5379e51b6c8be5f7aa009f5bb8c77629305d4
Susmit Vengurlekar <susmit.py@gmail.com>
2022-02-14 13:17:50 +0530

    ptrs

--- longest_palin_sub_str.py
+++ longest_palin_sub_str.py
@@ -1,8 +1,13 @@
+from typing import List, Tuple
+
+
 class Solution:
     non_palindromic_strs = set()
-    
+
     def is_palindrome(self, s: str, len_s: int) -> bool:
+        # print("Checking if palindrome")
         # print(s)
+        # print(len_s)
         start_ptr = 0
         end_ptr = len_s - 1
         while start_ptr < end_ptr:
@@ -12,35 +17,45 @@ class Solution:
             end_ptr -= 1
 
         return True
-    
-    def get_longest_child_palindrome(self, ls: list[str], len_s: int):
-        children = []
-        for s in ls:
-            children.extend([s[1:], s[:-1]])
 
-            if s in self.non_palindromic_strs:
+    def get_longest_child_palindrome(
+        self, s: str, ptrs: List[Tuple[int, int]]
+    ):
+        children_ptrs = []
+
+        # print("Ptrs to check")
+        # print(ptrs)
+
+        for left_ptr, right_ptr in ptrs:
+            children_ptrs.extend([(left_ptr + 1, right_ptr), (left_ptr, right_ptr - 1)])
+
+            str_to_check = s[left_ptr : right_ptr + 1]
+            if str_to_check in self.non_palindromic_strs:
                 continue
-                
-            if self.is_palindrome(s, len_s):
-                return s
-            else:
-                self.non_palindromic_strs.add(s)
 
-        return self.get_longest_child_palindrome(children, len_s-1)
+            if self.is_palindrome(str_to_check, (right_ptr - left_ptr) + 1):
+                return str_to_check
+            else:
+                self.non_palindromic_strs.add(str_to_check)
+
+        return self.get_longest_child_palindrome(s, children_ptrs)
 
     def longestPalindrome(self, s: str) -> str:
+        # print(s)
         len_s = len(s)
         # Condition where the input string is itself a palindrome
         if self.is_palindrome(s, len_s):
             return s
-        longest_palindromic_str = self.get_longest_child_palindrome([s[1:], s[:-1]], len_s-1)
+        longest_palindromic_str = self.get_longest_child_palindrome(
+            s, [(0, len_s - 2), (1, len_s - 1)]
+        )
         return longest_palindromic_str
-                    
+
 
 s = Solution()
 
 assert s.is_palindrome("abba", 4) == True
-assert s.is_palindrome("bbabb",5) == True
+assert s.is_palindrome("bbabb", 5) == True
 assert s.is_palindrome("cad", 3) == False
 assert s.is_palindrome("a", 1) == True
 assert s.longestPalindrome("eabcb") == "bcb"
@@ -48,4 +63,4 @@ assert s.longestPalindrome("bb") == "bb"
 assert s.longestPalindrome("cbbd") == "bb"
 assert s.longestPalindrome("c") == "c"
 
-# s.longestPalindrome("babaddtattarrattatddetartrateedredividerb")
+s.longestPalindrome("babaddtattarrattatddetartrateedredividerb")
