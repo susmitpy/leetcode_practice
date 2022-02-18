6110657fcd2424dd5826bfb38ca2f3212429f83e
Susmit Vengurlekar <susmit.py@gmail.com>
2022-02-18 15:47:32 +0530

    add: longest common prefix

---
+++ container_with_most_water.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/container-with-most-water/
+from typing import List
+from json import load
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        areas = []
+        len_height = len(height)
+
+        for left_idx, left_height in enumerate(height[:-1]):
+            for right_idx in range(left_idx+1, len_height):
+                area = min(left_height, height[right_idx]) * (right_idx-left_idx)
+                areas.append(area)
+
+        return max(areas)
+
+
+s = Solution()
+assert s.maxArea([1,8,6,2,5,4,8,3,7]) == 49
+assert s.maxArea([1,1]) == 1
+
+inputs = load(open("./inputs/container_with_most_water.json"))
+assert s.maxArea(inputs["0"]) == 48431514
---
+++ longest_common_prefix.py
@@ -0,0 +1,46 @@
+from typing import List
+
+class Solution:
+    def get_common_prefix(self, str1: str, str2: str):
+        common_prefix_last_index = -1
+        for s1, s2 in zip(str1, str2):
+            if s1 == s2:
+                common_prefix_last_index += 1
+            else:
+                break
+        
+        if common_prefix_last_index == -1:
+            return ""
+        
+        return str1[:common_prefix_last_index+1]
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # Check if there is only one element in the input list
+        try:
+            second_elem = strs[1]
+        except Exception:
+            # There is only one element
+            return strs[0]
+
+        if strs[0] == "":
+            return ""
+
+        len_strs = len(strs)
+        longest_common_prefix = strs[0]
+
+        for i in range(1,len_strs):
+            longest_common_prefix = self.get_common_prefix(longest_common_prefix, strs[i])
+            if longest_common_prefix == "":
+                return ""
+
+        return longest_common_prefix
+
+
+s = Solution()
+
+assert s.get_common_prefix("susmit", "susm") == "susm"
+assert s.get_common_prefix("susmit", "susmits") == "susmit"
+assert s.get_common_prefix("dog", "racecar") == ""
+
+assert s.longestCommonPrefix(["flower","flow","flight"]) == "fl"
+assert s.longestCommonPrefix(["dog","racecar","car"]) == ""
