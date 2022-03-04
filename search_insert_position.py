ed9686f98e81f3aaecbbaefbbcde65e830358cc3
Susmit Vengurlekar <susmit.py@gmail.com>
2022-03-04 16:14:02 +0530

    fix: search insert position

--- search_insert_position.py
+++ search_insert_position.py
@@ -1,55 +1,36 @@
 from typing import List
-from math import ceil
 
 class Solution:
     search_space: List[int]
     lookout_for: int
     len_search_space: int
 
-    def search(self, left_idx: int, right_idx: int):
-        if right_idx < 0:
-            return 0
+    def search(self, start: int, end: int) -> int:
+        midpoint = (start+end) // 2
+        if start > end: 
+            return start
 
-        middle_point = ceil((right_idx+left_idx)/2)
+        elem = self.search_space[midpoint]
+        if elem == self.lookout_for:
+            return midpoint
+        
+        if elem < self.lookout_for:
+            return self.search(start=midpoint+1, end=end)
 
-        if middle_point >= self.len_search_space:
-            return middle_point
-
-        middle_elem = self.search_space[middle_point]
-        if middle_elem == self.lookout_for:
-            print("Target Found")
-            return middle_point
-
-        if left_idx == right_idx:
-            print("Target Not Found")
-
-            curr_one_is_greater = self.search_space[left_idx] > self.lookout_for
-            if curr_one_is_greater:
-                return left_idx
-            else:
-                return left_idx+1
-
-        if middle_elem > self.lookout_for:
-            return self.search(left_idx, middle_point-1)
-
-        return self.search(middle_point+1, right_idx)
-
+        return self.search(start=start, end=midpoint-1)
+        
     def searchInsert(self, nums: List[int], target: int) -> int:
         self.search_space = nums
         self.lookout_for = target
         self.len_search_space = len(nums)
 
-        return self.search(0, len(nums)-1)
+        return self.search(start=0, end=len(nums)-1)
 
 
 s = Solution()
-
 assert s.searchInsert([1,3,5,6], 5) == 2
 assert s.searchInsert([1,3,5,6], 2) == 1
 assert s.searchInsert([1,3,5,6], 7) == 4
 assert s.searchInsert([1,3,5,6], 0) == 0
 assert s.searchInsert([1,3], 0) == 0
 assert s.searchInsert([1,3], 2) == 1, s.searchInsert([1,3], 2)
-s.searchInsert([1,2,4,6,7], 3)
-s.searchInsert([3,5,7,9,10], 8)
-s.searchInsert([1,3],4)
